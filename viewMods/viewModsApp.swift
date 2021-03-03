9c2ed358804ae4d9e81565808d9112490bef4c5d
Kat Allee <kallee@butlercc.edu>
2021-03-03 09:04:03 -0600

    Initial Commit

---
+++ viewMods/viewModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewModsApp.swift
+//  viewMods
+//
+//  Created by Student on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct viewModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
